bec7cd15bc289e3182c32f1f870e54f1dcf2ed65
Philipp <Philipp@192.168.1.102>
2012-12-08 21:58:27 +0100

    hallo

---
+++ test/src/testv1/Ausgabe.java
@@ -0,0 +1,16 @@
+package testv1;
+
+public class Ausgabe {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("XDtEst");
+		
+		
+		
+	}
+
+}
